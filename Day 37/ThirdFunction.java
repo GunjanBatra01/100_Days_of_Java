65f79b613ab64d72fb6ab62abd4735488f0408ba
GunjanBatra01 <85386436+GunjanBatra01@users.noreply.github.com>
2022-12-23 20:01:56 +0530

    concat() function

---
+++ Day 37/ThirdFunction.java
@@ -0,0 +1,14 @@
+public class ThirdFunction {
+    public static void main(String[] args) {
+        String str1 = "100";  
+        String str2 = "Days";  
+        String str3 = "of";
+        String str4 = "Challenge";  
+        String str5 = str1.concat(" ").concat(str2).concat(" ").concat(str3).concat(" ").concat(str4);  
+        System.out.println(str5);              
+        String str6 = str1.concat(" !!!");  
+        System.out.println(str6);         
+        String str7 = str1.concat(" ").concat(str2).concat(" ").concat(str3).concat(" ").concat("Coding");  
+        System.out.println(str7);
+    }
+}
