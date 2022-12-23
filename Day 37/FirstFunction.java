d8d163b52fe44faf8e4762a9f1ed8a1b454401a5
GunjanBatra01 <85386436+GunjanBatra01@users.noreply.github.com>
2022-12-23 20:00:35 +0530

    charAt() method

---
+++ Day 37/FirstFunction.java
@@ -0,0 +1,9 @@
+// charAt(int index) method
+
+public class FirstFunction{
+    public static void main(String[] args) {
+        String str = "This is charAt method of String";
+        char ch = str.charAt(3);
+        System.out.println(ch);
+    }
+}
