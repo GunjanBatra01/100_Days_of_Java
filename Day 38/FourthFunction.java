9fd934ed9cc3d2b54abae756d1a9d78ea4a65bf7
GunjanBatra01 <85386436+GunjanBatra01@users.noreply.github.com>
2022-12-23 22:19:06 +0530

    contains() method

---
+++ Day 38/FourthFunction.java
@@ -0,0 +1,8 @@
+public class FourthFunction{
+    public static void main(String[] args) {
+        String str = "This is my 38th Day";
+        boolean isContains = str.contains("is");
+        System.out.println(isContains);
+        System.out.println(str.contains("Is"));
+    }
+}
