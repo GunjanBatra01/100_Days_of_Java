80601beedf5f5bd81ed0aecf0967e331ab5c608d
GunjanBatra01 <85386436+GunjanBatra01@users.noreply.github.com>
2023-02-15 22:47:36 +0530

    Real Life Problem

---
+++ Day 70/arr.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class arr
+{
+    public static void main(String Args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s;
+        s=sc.nextLine();
+        System.out.println(s);
+    }
+}
