b43c4769a0295ae4e639be374f9db741ad7488ed
GunjanBatra01 <85386436+GunjanBatra01@users.noreply.github.com>
2023-03-10 16:12:27 +0530

    Real Life Problem

---
+++ Day 71/lower.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class lower
+{
+    public static void main(String Args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s;
+        s=sc.nextLine();
+        System.out.println(s.toLowerCase());
+    }
+}
